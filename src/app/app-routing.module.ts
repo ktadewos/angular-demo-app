0dc8795d716cabd87714e8441fac12bf8c25e652
ktadewos <kaleab.ero@mediaagility.com>
2021-08-11 00:26:07 -0500

    create a shell component also other components and create routing for them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './components/book/add-book/add-book.component';
+import { BookDetailComponent } from './components/book/book-detail/book-detail.component';
+import { BookListComponent } from './components/book/book-list/book-list.component';
+import { ShellComponent } from './components/shell/shell.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ShellComponent,
+    children: [
+      {
+        path:'book/list',
+        component: BookListComponent
+      },
+      {
+        path:'book/add',
+        component: AddBookComponent
+      },
+      {
+        path:'book/detail',
+        component: BookDetailComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
