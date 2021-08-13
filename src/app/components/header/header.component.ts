f93c0a9e5e50cab919830acf2f66a569ba6b36ad
ktadewos <kaleab.ero@mediaagility.com>
2021-08-13 08:51:56 -0500

    implement logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/model/user';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,10 +12,16 @@ export class HeaderComponent implements OnInit {
 
   user: User = {};
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService:AuthService, private route:Router) { }
 
   ngOnInit(): void {
     this.user = this.authService.getCurrentUser();
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('userData');
+    this.route.navigate(['/login'])
   }
 
 }
